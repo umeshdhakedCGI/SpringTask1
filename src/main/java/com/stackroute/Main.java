b3e20acbe549d0bddebe070387a9b450cbdf365e
umeshdhakedCGI <umesh.dhaked@cgi.com>
2019-10-01 17:34:03 +0530

    task 3

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -6,7 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Task 2
+ * Task 3
  */
 public class Main
 {
