cc6ac52e474b1b3e84ea02621110e9a57ef7b187
umeshdhakedCGI <umesh.dhaked@cgi.com>
2019-10-02 04:01:41 +0530

    constructor changed in xml

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -8,27 +8,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class Movie {
 
     @Autowired
-    private Actor actor;
+    private Actor actor3;
 
     public Movie(){
-
+        System.out.println("cons");
     }
 
-    public Movie(Actor actor){
-        this.actor = actor;
+    public Movie(Actor actor3){
+        this.actor3 = actor3;
+        System.out.println("con args");
     }
 
 
-public void setActor(Actor actor){
-    this.actor = actor;
+public void setActor(Actor actor3){
+    this.actor3 = actor3;
+    System.out.println("lol");
 }
 
 
 public void getMovieInfo(){
 
-    actor.getName();
-    actor.getAge();
-    actor.getGender();
+    actor3.getName();
+    actor3.getAge();
+    actor3.getGender();
 
 }
 
